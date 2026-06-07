be6c49489cf179a665ad3f00a66d740059f83f1b
Sameer Ali <140313541+devxsameer@users.noreply.github.com>
2026-06-07 12:37:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
-    //Write your code here
+    event.preventDefault();
 
+    const fname = document.forms["form1"]["fname"].value.trim();
+    const lname = document.forms["form1"]["lname"].value.trim();
+
+    alert(fname + " " + lname);
 }
